793d3d41c368a656f5d9e4d4eeef470e4eef0f20
Tuanse160549 <tuannvse160549@fpt.edu.vn>
2024-06-19 16:33:27 +0700

    Cập nhật tính năng mới

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Homepage from "./Pages/Homepage";
+import Ring from "./Pages/Ring";
+import DiamondPage from "./Pages/DiamondPage";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
+import Admin from "./Admin/Admin";
+import Category from "./Admin/Elements/Category/Category";
+import Account from "./Admin/Elements/Account/Account";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Homepage from './Pages/Homepage'
-import Ring from './Pages/Ring'
-import DiamondPage from './Pages/DiamondPage'
-import Login from './Auth/Login'
-import SignUp from './Auth/SignUp'
-// import LogoutAndRedirect from './Auth/AuthFucntion'
-
-import Category from './Admin/Elements/Category/Category'
-import Account from './Admin/Elements/Account/Account'
-import Admin from './Admin/Admin'
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        {/* <LogoutAndRedirect /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Homepage />} />
+          <Route path="diamondPage" element={<DiamondPage />} />
+          <Route path="ring" element={<Ring />} />
+        </Route>
 
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route index element={<Homepage />} />
-            <Route path='diamondPage' element={<DiamondPage />}></Route>
-            <Route path='ring' element={<Ring />}></Route>
-          </Route>
-
-
-          {/* <Route path='/a' element={<Home />}></Route>
-          <Route path='/b' element={<Home />}></Route>
-          <Route path='/c' element={<Home />}></Route>
-          <Route path='/d' element={<Home />}></Route>
-          <Route path='/e' element={<Home />}></Route> */}
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/signup' element={<SignUp />}></Route>
-          <Route path='/admin' element={<Admin />}>
-            <Route path='category' element={<Category />}></Route>
-            <Route path='account' element={<Account />}></Route>
-          </Route>
-
-        </Routes >
-      </BrowserRouter >
-
-    </>
-  )
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin" element={<Admin />}>
+          <Route path="category" element={<Category />} />
+          <Route path="account" element={<Account />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,91 +1,82 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import "./Login.css"
-import { useState, useEffect } from 'react';
-import { validateUser } from './AuthFucntion';
-import { jwtDecode } from 'jwt-decode';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+import { useState } from "react";
+import { validateUser } from "./AuthFucntion";
+import { jwtDecode } from "jwt-decode";
+
 export default function Login() {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   let navigate = useNavigate();
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      if (decodedToken.Role === '1') {
-        navigate('/admin');
-      } else {
-        navigate('/');
-      }
-    }
-  }, [navigate]);
 
-  function getToken() {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      if (decodedToken.Role === '1') {
-        navigate('/admin');
-      } else {
-        navigate('/');
-      }
-    }
-  }
-
-  const submitForm = e => {
-    e.preventDefault()
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);  // In giá trị của email
+    console.log("Password:", password); // In giá trị của password
     validateUser({ email: email, password: password })
-      .then(() => {
-        getToken()
+      .then((response) => {
+        // Kiểm tra và chuyển hướng sau khi đăng nhập thành công
+        const token = localStorage.getItem("token");
+        if (token) {
+          const decodedToken = jwtDecode(token);
+          console.log("decodedToken", decodedToken); // Kiểm tra decodedToken
+          if (decodedToken.RoleID === "1") { // Kiểm tra RoleID
+            console.log("admin")
+            navigate("/admin"); // Chuyển hướng đến /admin
+             
+          } else {
+            console.log("out")
+            navigate("/"); // Chuyển hướng về trang Home
+          }
+        }
       })
-      .catch(error => {
+      .catch((error) => {
+        console.error("Login error:", error);
         setError(error.message);
-      })
-  }
+      });
+  };
+
   return (
-    <section className='pageLoginContainer' >
-      <div className='loginContainer container-fluid'>
+    <section className="pageLoginContainer">
+      <div className="loginContainer container-fluid">
         <h1>LOGIN TO DASHBOARD</h1>
         {error && <div className="error-message">{error}</div>}
-        <div className='loginInputContainer'>
+        <div className="loginInputContainer">
           <form onSubmit={submitForm}>
             <input
               type="text"
               placeholder="Enter your email here"
-              className='form-control'
+              className="form-control"
               value={email}
-              onChange={e => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               placeholder="Password"
-              className='form-control'
+              className="form-control"
               value={password}
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <div className='forgotPassword'>
-              <Link to="/" className='linkForgotPassword'>
+            <div className="forgotPassword">
+              <Link to="/" className="linkForgotPassword">
                 Forgot password?
               </Link>
             </div>
-            <button className='loginButton'>LOGIN</button>
+            <button className="loginButton">LOGIN</button>
           </form>
-          <Link to="/SignUp" className='signupLink'>
-            <button className='signUpButton'>
-              SIGN UP
-            </button>
+          <Link to="/SignUp" className="signupLink">
+            <button className="signUpButton">SIGN UP</button>
           </Link>
 
-          <div className='backToHomePageLink'>
-            <Link to="/" className='linkBackHome'>
+          <div className="backToHomePageLink">
+            <Link to="/" className="linkBackHome">
               Back to homepage
             </Link>
           </div>
         </div>
       </div>
-    </section >
-  )
+    </section>
+  );
 }
-
